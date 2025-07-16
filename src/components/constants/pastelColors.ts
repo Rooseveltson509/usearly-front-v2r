c0163497977abcac4740b97763661a70258995cc
cebeat <rooseveltsonc@gmail.com>
2025-07-16 18:39:21 +0200

    Résolution du conflit avec master

--- src/components/constants/pastelColors.ts
+++ src/components/constants/pastelColors.ts
@@ -5,6 +5,7 @@ export const pastelColors = [
   "rgb(247 247 255)", // jaune très clair
   "rgb(247 247 255)", // gris clair
 ];
+
 export const pastelColorsDark = [
   "#F0F8FF", // bleu foncé
   "#065F46", // vert foncé
@@ -12,6 +13,7 @@ export const pastelColorsDark = [
   "#9D174D", // rose foncé
   "#374151", // gris foncé
 ];
+
 export const pastelColorsLight = [
   "#E0F2FE", // bleu clair
   "#D1FAE5", // vert clair
@@ -19,6 +21,7 @@ export const pastelColorsLight = [
   "#FDE2E8", // rose clair
   "#F3F4F6", // gris clair
 ];
+
 export const pastelColorsMuted = [
   "#B0C4DE", // bleu pâle
   "#98FB98", // vert pâle
