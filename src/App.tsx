1f3a4923fa6e1448f9dc580e33acde2ca996b3c8
cebeat <rooseveltsonc@gmail.com>
2025-07-18 17:01:01 +0200

    feature: test maincontent

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import RequestResetPassword from "./pages/forgot-and-reset-pwd/RequestResetPassw
 import ResetPassword from "./pages/forgot-and-reset-pwd/ResetPassword";
 import NavigateToHome from "./pages/NavigateToHome";
 import NewHome from "./pages/newHome/NewHome";
+import { MainContent } from "./pages/MainContent";
 
 function App() {
   return (
@@ -86,6 +87,7 @@ function App() {
               }
             />
             <Route path="/home" element={<NewHome />} />
+            <Route path="/impact" element={<MainContent activeTab={"report"} />} />
             <Route
               path="/feedback"
               element={
