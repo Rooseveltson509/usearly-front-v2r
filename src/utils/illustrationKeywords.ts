b41313e467a89eeb40afb10b8139c1a70c5527c1
cebeat <rooseveltsonc@gmail.com>
2025-10-10 16:21:42 +0200

    feature: add matching illustration to puncheline carte

---
+++ src/utils/illustrationKeywords.ts
@@ -0,0 +1,83 @@
+export const illustrationKeywords: Record<string, string> = {
+  // 🏦 Banque
+  banque: "bancaire/cartecredit.svg",
+  paiement: "bancaire/cartecredit.svg",
+  carte: "bancaire/cartecredit.svg",
+  crédit: "bancaire/cartecredit.svg",
+  invest: "bancaire/invest.svg",
+  économie: "bancaire/tirelire.svg",
+  récompense: "bancaire/tirelire.svg",
+  points: "bancaire/tirelire.svg",
+
+  // 🎬 TV & Cinéma
+  cinéma: "tv_cinema/clapcinema.svg",
+  film: "tv_cinema/clapcinema.svg",
+  popcorn: "tv_cinema/popcorn.svg",
+  canapé: "tv_cinema/canape.svg",
+  série: "tv_cinema/canape.svg",
+
+  // 🎧 Culture & Musique
+  musique: "culture_musique/notemusique.svg",
+  chanson: "culture_musique/notemusique.svg",
+  concert: "culture_musique/rocksigne.svg",
+  livre: "culture_musique/openbook.svg",
+  lecture: "culture_musique/openbook.svg",
+  casque: "culture_musique/casquemusique.svg",
+
+  // 🍔 Food & Drink
+  repas: "food_drink/burger.svg",
+  restaurant: "food_drink/burger.svg",
+  livraison: "food_drink/livraisonvelo.svg",
+  colis: "food_drink/livraisonvelo.svg",
+  vélo: "food_drink/livraisonvelo.svg",
+  smoothie: "food_drink/smoothie.svg",
+  cocktail: "food_drink/cocktail.svg",
+  glace: "food_drink/ice.svg",
+
+  // 🛒 E-commerce
+  commande: "ecommerce/caddie.svg",
+  achat: "ecommerce/fillbag.svg",
+  panier: "ecommerce/caddie.svg",
+  produit: "ecommerce/mediumbag.svg",
+  magasin: "ecommerce/largebag.svg",
+  validation: "ecommerce/fillbag.svg",
+
+  // 🏨 Hôtellerie & immobilier
+  hôtel: "hotelerie_immobilier/lit.svg",
+  chambre: "hotelerie_immobilier/lit.svg",
+  clé: "hotelerie_immobilier/clef.svg",
+  location: "hotelerie_immobilier/clef.svg",
+
+  // ✈️ Voyage & transport
+  avion: "voyage_transport/avion.svg",
+  vol: "voyage_transport/avion.svg",
+  trajet: "voyage_transport/chemin.svg",
+  train: "voyage_transport/train.svg",
+  valise: "voyage_transport/valise.svg",
+  transport: "voyage_transport/valise.svg",
+
+  // ⚽ Sport
+  sport: "sport/maillot.svg",
+  basket: "sport/basketball.svg",
+  foot: "sport/cagefoot.svg",
+  course: "sport/chaussurecourse.svg",
+  cible: "sport/cible.svg",
+  podium: "sport/podium.svg",
+  sac: "sport/sacsport.svg",
+
+  // 🌈 UI / Expérience
+  interface: "general/ordilike.svg",
+  design: "general/ordilike.svg",
+  animation: "general/ordilike.svg",
+  rapidité: "general/flèchecoeur.svg",
+  performance: "general/flèchecoeur.svg",
+
+  // 💬 Généraliste / fallback
+  like: "general/coeurlike.svg",
+  coeur: "general/coeurlike.svg",
+  love: "general/coeurdoigt.svg",
+  bravo: "general/applaudissement.svg",
+  rire: "general/emojiteterrire.svg",
+  message: "general/messagelike.svg",
+  default: "general/ordilike.svg",
+};
