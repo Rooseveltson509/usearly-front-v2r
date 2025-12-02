5879972ee1f030aa23c47044c8e6d12e46650d9a
Zeyoman <alex10pump@gmail.com>
2025-12-02 17:36:25 +0100

    style: landing exprimer vous bientot fini verif christine

--- src/pages/newHome/components/extensionExample/ExtensionExample.tsx
+++ src/pages/newHome/components/extensionExample/ExtensionExample.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import "./extensionExample.scss";
 import ExtensionExampleText from "./extensionExampleText/ExtensionExampleText";
 import ExtensionExampleImage from "./extensionExampleImage/ExtensionExampleImage";
+import type { ExtensionScenario } from "./extensionExample.types";
+import bigUofUsearly from "/assets/icons/bigUofUsearly.svg";
 
 const ExtensionExample = () => {
+  const [selectedScenario, setSelectedScenario] =
+    useState<ExtensionScenario>("signalez");
+
   return (
     <div className="extension-example-container">
-      <ExtensionExampleText />
-      <ExtensionExampleImage />
+      <ExtensionExampleText
+        selected={selectedScenario}
+        setSelected={setSelectedScenario}
+      />
+      <ExtensionExampleImage selected={selectedScenario} />
+      <img className="img2" src={bigUofUsearly} alt="U of Usearly" />
     </div>
   );
 };
--- src/pages/newHome/components/extensionExample/extensionExampleImage/ExtensionExampleImage.tsx
+++ src/pages/newHome/components/extensionExample/extensionExampleImage/ExtensionExampleImage.tsx
@@ -1,20 +1,127 @@
+import { useEffect, useState } from "react";
 import "./ExtensionExampleImage.scss";
-import ExtensionTabSignalement from "/assets/images/extensionImageSignalement.svg";
-import { LogoBig } from "@src/components/shared/DecorativeLogos";
+import ExtensionTabSignalementBase from "/assets/images/Landing/extensionsReportDescription.svg";
+import ExtensionTabSingalementSearch from "/assets/images/Landing/extensionpopupReportSearch.svg";
+import ExtensionTabSingalementSearchValue from "/assets/images/Landing/extensionPopUpReportSearchValue.svg";
+import ExtensionTabSingalementThanks from "/assets/images/Landing/extensionPopupReportThanks.svg";
+import ExtensionPopupReportReponse from "/assets/images/Landing/extensionPopupReportReponse.svg";
+import ExtensionPopupReportSolution from "/assets/images/Landing/extensionPopupReportSolution.svg";
+import ExtensionPopupReportSuivi from "/assets/images/Landing/extensionPopupReportSuivi.svg";
+import ImgReport from "/assets/images/Landing/webImgReport.svg";
+import ExtensionBarSignalement from "/assets/images/Landing/extensionBarReport.svg";
+import ExtensionBarLike from "/assets/images/Landing/extensionBarLike.svg";
+import ExtensionPopupLike from "/assets/images/Landing/extensionPopupLike.svg";
+import ExtensionPopupLikeThanks from "/assets/images/Landing/extensionPopupLikeThanks.svg";
+import ExtensionPopupLikePublication from "/assets/images/Landing/extensionPopupLikePublication.svg";
+import ImgLike from "/assets/images/Landing/likeBackgroundWebImg.svg";
+import ExtensionBarSuggest from "../../../../../../public/assets/images/Landing/extensionBarSuggest.svg";
+import ExtensionPopupSuggest from "/assets/images/Landing/extensionPopupSuggest.svg";
+import ExtensionPopupSuggestThanks from "/assets/images/Landing/extensionPopupSuggestThanks.svg";
+import ExtensionPopupSuggestPublication from "/assets/images/Landing/extensionPopupSuggestPublication.svg";
+import ImgSuggest from "/assets/images/Landing/suggestBackgroundWebImg.svg";
+import type { ExtensionScenario } from "../extensionExample.types";
 
-const ExtensionExampleImage = () => {
+const etapePopUpExtensionReport: string[] = [
+  ExtensionTabSignalementBase,
+  ExtensionTabSingalementSearch,
+  ExtensionTabSingalementSearchValue,
+  ExtensionTabSingalementThanks,
+  ExtensionPopupReportReponse,
+  ExtensionPopupReportSolution,
+  ExtensionPopupReportSuivi,
+];
+
+const etapePopUpExtensionLike: string[] = [
+  ExtensionPopupLike,
+  ExtensionPopupLikeThanks,
+  ExtensionPopupLikePublication,
+];
+
+const etapePopUpExtensionSuggest: string[] = [
+  ExtensionPopupSuggest,
+  ExtensionPopupSuggestThanks,
+  ExtensionPopupSuggestPublication,
+];
+
+const etapePopupsByScenario: Record<ExtensionScenario, string[]> = {
+  signalez: etapePopUpExtensionReport,
+  félicitez: etapePopUpExtensionLike,
+  suggérez: etapePopUpExtensionSuggest,
+};
+
+const etapeBarExtension: Record<ExtensionScenario, string> = {
+  signalez: ExtensionBarSignalement,
+  félicitez: ExtensionBarLike,
+  suggérez: ExtensionBarSuggest,
+};
+
+const STEP_DURATION_MS = 2200;
+
+type ExtensionExampleImageProps = {
+  selected: ExtensionScenario;
+};
+
+const ExtensionExampleImage = ({ selected }: ExtensionExampleImageProps) => {
+  const [etape, setEtape] = useState<number>(0);
+  const etapePopUps = etapePopupsByScenario[selected];
+  const totalEtapes = etapePopUps.length;
+  const safeEtapeIndex = Math.min(etape, totalEtapes - 1);
+  const stepKey = `${selected}-${safeEtapeIndex}`;
+
+  useEffect(() => {
+    setEtape(0);
+  }, [selected]);
+
+  useEffect(() => {
+    if (totalEtapes <= 1) return;
+
+    const timer = setInterval(() => {
+      setEtape((prevEtape) => (prevEtape + 1) % totalEtapes);
+    }, STEP_DURATION_MS);
+
+    return () => clearInterval(timer);
+  }, [totalEtapes, selected]);
+
+  let addClass = "";
+  let addBgClass = "";
+  let imgBackground = "";
+  if (selected === "suggérez") {
+    addClass = "suggestion-mode e" + etape;
+    imgBackground = ImgSuggest;
+    addBgClass = "suggestion-bg-mode";
+  } else if (selected === "félicitez") {
+    addClass = "like-mode e" + etape;
+    imgBackground = ImgLike;
+    addBgClass = "like-bg-mode";
+  } else {
+    addClass = "report-mode e" + etape;
+    imgBackground = ImgReport;
+    addBgClass = "report-bg-mode";
+  }
+
   return (
     <div className="extension-example-image">
-      <img
-        src={ExtensionTabSignalement}
-        alt="Extension Example"
-        className="extension-image"
-      />
-      <LogoBig
-        className="logo-big-landing-page"
-        strokeColor="#4549ef29"
-        size={1.7}
-      />
+      <div className={"imgWebExample " + addBgClass}>
+        <img
+          src={imgBackground}
+          alt="Extension Example"
+          className="web-image"
+        />
+      </div>
+      <div className="extensionImg">
+        <img
+          key={stepKey}
+          src={etapePopUps[safeEtapeIndex]}
+          alt="Extension popup Example"
+          // className={"extension-image " + addClass}
+          className={"extension-image step-animation " + addClass}
+        />
+        <img
+          src={etapeBarExtension[selected]}
+          alt="Extension bar Example"
+          className={"extension-image-bar " + addClass}
+        />
+      </div>
     </div>
   );
 };
